536be5af02e31081e6fbdae2d9290955a760967a
amerikv220 <125311200+amerikv220@users.noreply.github.com>
2023-08-29 17:02:52 -0300

    Update MainViewModelUnitTest.kt

--- app/src/test/java/com/example/minuevaapp/MainViewModelUnitTest.kt
+++ app/src/test/java/com/example/minuevaapp/MainViewModelUnitTest.kt
@@ -4,11 +4,6 @@ package com.example.minuevaapp
 import org.junit.Test
 import org.junit.Before
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 
 
 
@@ -19,20 +14,19 @@ class MainViewModelUnitTest {
 
     @Before
     fun setup() {
-        // Inicializar el ViewModel
+        // Inicializamos el ViewModel
         viewModel = MainViewModel()
     }
 
     @Test
     fun testCompareTextsEqual() {
-        // Preparar el escenario de prueba
+     
         val text1 = "Hola"
         val text2 = "Hola"
 
-        // Ejecutar la función que queremos probar
+   
         val result = viewModel.compareTexts(text1, text2)
 
-        // Verificar el resultado
         assert(result)
     }
 
@@ -42,10 +36,10 @@ class MainViewModelUnitTest {
         val text1 = "Hola"
         val text2 = "Adiós"
 
-        // Ejecutar la función que queremos probar
+    
         val result = viewModel.compareTexts(text1, text2)
 
-        // Verificar el resultado
+     
         assert(!result)
     }
 }
